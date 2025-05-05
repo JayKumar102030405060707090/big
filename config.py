10a533d14bc2f1b9875c40a36bfaa606b0637abf
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-05-05 14:17:17 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+# Bot Token
+TOKEN = "YOUR_BOT_TOKEN"
+
+# MongoDB URI
+MONGODB_URI = "mongodb+srv://username:password@cluster.mongodb.net/?retryWrites=true&w=majority"
+
+# Channel Link for Forced Subscribe
+CHANNEL_LINK = "https://t.me/your_channel"
+
+# Admin ID (Owner of the Bot)
+ADMIN_ID = 7168729089
+
+# MongoDB collection for vote data
+VOTE_COLLECTION = "votes"
