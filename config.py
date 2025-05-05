5891f416459dc22d7b76a78992ab4a0cf15a7fd3
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-05-05 14:21:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 # Bot Token
-TOKEN = "YOUR_BOT_TOKEN"
+TOKEN = "7745907443:AAFM4XhKW4rBjLzdOm-e1mB0HlJdo-t8lYM"
 
 # MongoDB URI
-MONGODB_URI = "mongodb+srv://username:password@cluster.mongodb.net/?retryWrites=true&w=majority"
+MONGODB_URI = "mongodb+srv://jaydipmore74:xCpTm5OPAfRKYnif@cluster0.5jo18.mongodb.net/?retryWrites=true&w=majority"
 
 # Channel Link for Forced Subscribe
-CHANNEL_LINK = "https://t.me/your_channel"
+CHANNEL_LINK = "https://t.me/komalMusicUpdate"
 
 # Admin ID (Owner of the Bot)
 ADMIN_ID = 7168729089
