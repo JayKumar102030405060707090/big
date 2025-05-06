29a5191b2e13bb2181c53a166a6956bc93e96b79
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-05-07 00:54:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,9 @@
-import os
-
-# Bot Token
-TOKEN = "7745907443:AAFM4XhKW4rBjLzdOm-e1mB0HlJdo-t8lYM"
-
-# MongoDB URI
-MONGODB_URI = "mongodb+srv://jaydipmore74:xCpTm5OPAfRKYnif@cluster0.5jo18.mongodb.net/?retryWrites=true&w=majority"
-
-# Channel Link for Forced Subscribe
-CHANNEL_LINK = "https://t.me/komalMusicUpdate"
-
-# Admin ID (Owner of the Bot)
-ADMIN_ID = 7168729089
-
-# MongoDB collection for vote data
-VOTE_COLLECTION = "votes"
+class Config:
+    API_ID = 12380656
+    API_HASH = "d927c13beaaf5110f25c505b7c071273"
+    BOT_TOKEN = "7745907443:AAFM4XhKW4rBjLzdOm-e1mB0HlJdo-t8lYM"
+    MONGODB_URI = "mongodb+srv://jaydipmore74:xCpTm5OPAfRKYnif@cluster0.5jo18.mongodb.net/?retryWrites=true&w=majority"
+    UPDATE_CHANNEL = "KOMALMUSICUPDATE"  # Channel users must join to use bot
+    SUPPORT_GROUP = "BestFriendsChattingZone"
+    BOT_OWNER = 7168729089
+    LOG_CHANNEL = -1001234567890  # Replace with your log channel ID
