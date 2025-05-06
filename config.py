67bbb28c4b286ef9f4755f7d28853bc5df7f7251
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-05-07 01:04:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     API_ID = 12380656
     API_HASH = "d927c13beaaf5110f25c505b7c071273"
-    BOT_TOKEN = "7745907443:AAFM4XhKW4rBjLzdOm-e1mB0HlJdo-t8lYM"
+    BOT_TOKEN = "7745907443:AAGcp5YbmRhjmZNv-Mto5nTINGXqYr-SCLI"
     MONGODB_URI = "mongodb+srv://jaydipmore74:xCpTm5OPAfRKYnif@cluster0.5jo18.mongodb.net/?retryWrites=true&w=majority"
     UPDATE_CHANNEL = "KOMALMUSICUPDATE"  # Channel users must join to use bot
     SUPPORT_GROUP = "BestFriendsChattingZone"
